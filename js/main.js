1d6e6971741ecdd2a60d76751228b61763cb7098
thanhlong <thanhlong98kun@gmail.com>
2019-01-18 21:26:43 +0700

    Dang Choi

--- js/main.js
+++ js/main.js
@@ -1,211 +1,138 @@
-﻿$(function () {
-
-    // preloader
-    var width = 100,
-        perfData = window.performance.timing,
-        EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart),
-        time = parseInt((EstimatedTime / 1000) % 60) * 100;
-
-    // Loadbar Animation
-    $(".loadbar").animate({
-        width: width + "%"
-    }, time);
-
-    // Loadbar Glow Animation
-    $(".glow").animate({
-        width: width + "%"
-    }, time);
-
-    // Percentage Increment Animation
-    var PercentageID = $("#precent"),
-        start = 0,
-        end = 100,
-        durataion = time;
-    animateValue(PercentageID, start, end, durataion);
-
-    function animateValue(id, start, end, duration) {
-
-        var range = end - start,
-            current = start,
-            increment = end > start ? 1 : -1,
-            stepTime = Math.abs(Math.floor(duration / range)),
-            obj = $(id);
-
-        var timer = setInterval(function () {
-            current += increment;
-            $(obj).text(current + "%");
-            //obj.innerHTML = current;
-            if (current == end) {
-                clearInterval(timer);
-            }
-        }, stepTime);
-    }
-
-    // Fading Out Loadbar on Finised
-    setTimeout(function(){
-      $('.preloader-wrap').fadeOut(300);
-    }, time);
-
-
-
-    // MENU
-    var $menuList2 = $('#menu-list2');
-    var $menuTM = new TimelineMax({
-        yoyoEase: true,
-        paused: true
+$(function () {
+    var tlmHome = new TimelineMax({
+        delay: 1
     });
-    $menuTM
-        .fromTo($menuList2, .5, {
-            y: "-100%"
-        }, {
-            y: "0%",
-            ease: Expo.easeOut
-        })
-        .staggerFrom('#menu-list2 li.nav-item', .4, {
-            cycle: {
-                x: [-100, 100]
-            },
-            opacity: 0
-        }, .2);
-
-    $('#btn-menu').click(function (e) {
-        e.preventDefault();
-        $(this).toggleClass('open');
-        $menuList2.toggleClass('showList2');
-        if ($menuList2.hasClass('showList2')) {
-            $menuTM.play().timeScale(1);
-        } else {
-            $menuTM.timeScale(3).reverse();
-        }
-        return false;
-    });
-
-    // CLICK VAO MENU
-    var $homePos = $('header').offset().top;
-
-    $menu_link2 = $('#menu-list2 .nav-link');
-    $menu_link2.click(function (e) {
-        e.preventDefault();
-        $('#btn-menu').removeClass('open');
-        $menuList2.removeClass('showList2');
-        $menuTM.timeScale(3).reverse();
-        let $idValue = $(this).data('id');
-        if ($idValue == '#home') {
-            TweenMax.to(window, .7, {
-                scrollTo: $homePos,
-                ease: Expo.easeIn,
-                delay: .8
-            });
-        } else {
-            TweenMax.to(window, .7, {
-                scrollTo: $idValue,
-                ease: Expo.easeIn,
-                delay: .8
-            });
-        }
-    })
-
-    var $menu_link1 = $('#menu-list1 .nav-link');
-    $menu_link1.click(function (e) {
-        e.preventDefault();
-        let $idValue = $(this).data('id');
-        if ($idValue == '#home') {
-            TweenMax.to(window, .7, {
-                scrollTo: $homePos,
-                ease: Expo.easeIn
-            });
-        } else {
-            TweenMax.to(window, .7, {
-                scrollTo: $idValue,
-                ease: Expo.easeIn
-            });
-        }
-    })
+    tlmHome
+        .add(hiThere())
+        .add(TweenMax.from('#star svg', 1.5, {
+            autoAlpha: 0,
+            scale: 0,
+            rotation: 60,
+            ease: Elastic.easeOut.config(2.5, 0.3)
+        }))
+        .add(TweenMax.to("#star .before, #star .after", 1.5, {
+            width: "12rem",
+            ease: Elastic.easeOut.config(1, 0.3)
+        }), '-=1.2')
+        .add(typed());
 
 
-
-    // **************************************************
-
-    var typed1 = new Typed("#text-run", {
-        strings: ["Chào mừng bạn đã đến với Website của tôi.", "Tôi là Nguyễn Thành Long.", "Tôi là sinh viên trường đại học Nha Trang.", "Kéo xuống và xem những thông tin về tôi nhé!!!"],
-        typeSpeed: 50,
-        backSpeed: 20,
-        backDelay: 5000,
-        cursorChar: '_',
-        smartBackspace: true,
-        loop: true,
-    });
-
-    $('[data-toggle="tooltip"]').tooltip();
-    // *****************************************************
-    // PROGRESS
-    var $progress_flags = false;
-    var $progress_time = 1.5;
-
-    function runProgress(obj) {
-        let $obj = $(obj);
-        let $width_bar = $obj.width();
-        let $value = $obj.attr('aria-valuenow');
-        let $bar_inner = $obj.find('.progress-bar');
-        let $text_inner = $bar_inner.next();
-        TweenMax.to($bar_inner, $progress_time, {
-            width: $value + "%",
-            ease: Bounce.easeOut,
-            onUpdate: changeIt,
-            onUpdateParams: [$width_bar, $bar_inner, $text_inner]
+    function hiThere() {
+        var tlm_hiThere = new TimelineMax({
+            onComplete: closeText
         });
-    }
-
-    function changeIt($width_bar, $bar_inner, $text_inner) {
-        let x = $bar_inner.width();
-        let i = Math.floor((x / $width_bar) * 100);
-        $text_inner.text(i + '%');
-
-    }
-
-    function runProgressAll($obj) {
-        $obj.each(function (index, el) {
-            runProgress(el);
-        })
-    }
-
-    var $thirdPos = $('#ab-content').offset().top;
-    $(window).scroll(function () {
-        var $bodyPos = $('html').scrollTop();
-        if ($bodyPos > 85) {
-            $('#menu').addClass('show-menu');
-        } else {
-            $('#menu').removeClass('show-menu');
+        let splitText = new SplitText("#hiThere", {
+            type: "words, chars"
+        });
+        let chars = splitText.chars;
+        for (let i = 0; i < chars.length; i++) {
+            tlm_hiThere
+                .from(chars[i], .8, {
+                    y: -100,
+                    autoAlpha: 0,
+                    ease: Bounce.easeOut,
+                }, Math.random() * 0.6);
         }
-        if ($bodyPos >= $thirdPos) {
-            if (!$progress_flags) {
 
-                runProgressAll($('.progress'));
-
-                $progress_flags = true;
-            }
+        function closeText() {
+            splitText.revert();
         }
+        return tlm_hiThere;
+    }
+
+    function typed() {
+        var tlm_typed = new TimelineMax({
+            repeat: -1
+        });
+        let item = $("#typed p");
+        for (let i = 0; i < item.length; i++) {
+            let text = new SplitText(item[i], {
+                type: "words, chars"
+            });
+            tlm_typed
+                .staggerFromTo(text.chars, .15, {
+                    autoAlpha: 0,
+                    y: 30,
+                }, {
+                    autoAlpha: 1,
+                    y: 0
+                }, .05)
+                .staggerTo(text.chars, .15, {
+                    opacity: 0,
+                    y: -30,
+                    delay: 3
+                }, .05);
+        }
+        return tlm_typed;
+    };
+
+
+    // function getTranslateX() {
+    //     var style = window.getComputedStyle($slider);
+    //     var matrix = new WebKitCSSMatrix(style.webkitTransform);
+    //     console.log('translateX', matrix.m41);
+    // }
+
+
+
+    // const $slider = $('#slider');
+    // var $walk = 0;
+    // var $w_slide = $('.slide').width();
+    // var $wm_slide = $('.slide').outerWidth();
+    // let isDown = false;
+
+
+    // $slider.on('mousedown touchstart', function (event) {
+    //     isDown = true;
+    //     var e = event || window.event;
+    //     var startX = e.pageX;
+    //     $slider.on('mousemove touchmove', function (event) {
+    //         if (!isDown) return;
+    //         var e = event || window.event;
+    //         $walk = e.pageX - startX;
+    //         TweenMax.to($slider,.1, {x: $walk} );
+    //     });
+    // });
+
+    // $slider.on('mouseup touchend', function (e) {
+    //     isDown = false;
+    //     $slider.off('mousemove touchmove');
+    //     if(Math.abs($walk) < $w_slide/2) {
+    //         TweenMax.to($slider, .4, {x: 0});
+    //     } else {
+    //         console.log($walk);
+    //         if($walk < 0) {
+    //             TweenMax.to($slider, .3, {x: -$wm_slide + 'px'});
+    //         }
+    //     }
+    // });
+
+
+    const $slider = document.getElementById('slider');
+    var isDown = false;
+    var startX;
+    var walk;
+
+    $slider.addEventListener('mousedown', function (event) {
+        isDown = true;
+        var e = event || window.event;
+        startX = e.pageX || e.originalEvent.touches[0].pageX;
     });
 
-    // END PROGRESS
-    // **********************************
-    // 
-    var $pf_list = $('#pf-list-item').isotope({
-        itemSelector: '.card',
-        // resizable: false,
-        masonry: {
-            columnWidth: 10,
-            fitWidth: true
-        }
+    $slider.addEventListener('mouseleave', function (event) {
+        isDown = false;
+    });
+    $slider.addEventListener('mouseup', function (event) {
+        isDown = false;
     });
 
-    $('#btn-pf li').click(function () {
-        var $filterValue = $(this).data('filter');
-        $pf_list.isotope({
-            filter: $filterValue,
-        })
-    })
-
+    $slider.addEventListener('mousemove', function (event) {
+        if (!isDown) return;
+        var e = event || window.event;
+        var x = e.pageX || e.originalEvent.touches[0].pageX;
+        walk = x - startX;
+        TweenMax.to($slider, .01, {x: walk});
+    });
 
 
 })
